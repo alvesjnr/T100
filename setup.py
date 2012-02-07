838e74dbd3085334382e6920b216ce9cabda9163
Antonio Ribeiro <alvejunior.antonio@gmail.com>
2012-02-07 19:22:06 +0100

    directories modification

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='SimulationFramework',
+      version='0.1',
+      description='My simulation Framework for my Msc',
+      author='Antonio Ribeiro',
+      author_email='alvesjunior.antonio@gmail.com',
+      url='alvesjnr.github.com',
+      packages=['src.framework'],
+     )
