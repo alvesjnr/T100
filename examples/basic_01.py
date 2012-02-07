63011973e280ab9d1af34fb64744006a6b6c812e
alvesjnr <alvesjunior.antonio@gmail.com>
2012-02-07 19:22:06 +0100

    directories modification

---
+++ examples/basic_01.py
@@ -0,0 +1,4 @@
+from simulator import *
+
+if __name__=='__main__':
+    print 'oi'
---
+++ t100/components.py
@@ -0,0 +1,115 @@
+import random
+import time
+
+class SimulationError(Exception):
+    def __init__(self, message=None):
+        self.message = message
+
+
+class QueueError(Exception):
+    def __init__(self, message=None):
+        self.message=message
+
+
+class Event(object):
+    
+    def __init__(self, timestamp=0):
+        self.timestamp = timestamp
+        self.__execution_behavior__()
+        self.locked=False
+    
+    def __repr__(self):
+        return '%s with timestamp %i>' % (str(self.__class__)[:-1], self.timestamp)
+    
+    def __execution_behavior__(self):
+        #just a placeholder, a placebo, must be overwrited
+        self.execution_time = random.randint(1,5)
+    
+    def __run__(self, parent):
+        #just a placeholder, a placebo, must be overwrited
+        parent.timestamp += self.execution_time
+
+
+class Queue(object):
+    
+    def __init__(self, max_capacity=None):
+        self.queue = []
+        self.max_capacity = max_capacity
+
+    def insert(self, event):
+        if self.max_capacity and len(self.queue) == self.max_capacity:
+            raise SimulationError("Queue %s reached maximum capacity" % (event))
+        if not isinstance(event,Event):
+            raise QueueError("Inserting non event object on queue")
+        
+        self.queue.append(event)
+        self.queue.sort(key=lambda event: event.timestamp)
+    
+    def remove(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            return self.queue.pop(0)
+    
+    def youngest_event(self):
+        if len(self.queue) > 0:
+            return self.queue[0].timestamp
+        else:
+            return None
+
+
+class Splitter(object):
+    
+    def __init__(self,number_of_outputs):
+        self.outputs = {}
+        self.number_of_outputs = number_of_outputs
+
+        for i in range(number_of_outputs):
+            self.outputs[i] = {'element':None}
+    
+    def link_output(self, output_number, queue):
+        self.outputs[output_number]['element'] = queue
+    
+    def insert(self, event):
+        if not isinstance(event, Event):
+            raise SplitterError("Inserting non event object on splitter")
+        
+        output_number = self.__get_splitter_output__()
+
+        if output_number is None:
+            """As __get_splitter_output__ was not implemented, uses default function"""
+            output_number = random.randint(0, len(self.outputs)-1) #TODO: must exist another way
+        
+        element = self.outputs[output_number]['element']
+        element.insert(event)
+    
+    def __get_splitter_output__(self):
+        """Not yet implemented"""
+
+
+class Source(object):
+
+    def __init__(self, output, timestamp=0):
+        self.timestamp = timestamp
+        self.output = output
+
+    def generate(self):
+
+        if random.random() > 0.5:
+            delta_t = random.randint(1,10)
+            self.output.insert(Event(timestamp=self.timestamp+delta_t))
+
+
+class Process(object):
+    def __init__(self, input=None, timestamp=0):
+        self.input = input
+        self.timestamp = timestamp
+    
+    def next(self):
+        if self.input and self.input.youngest_event() <= self.timestamp:
+            event = self.input.remove()            
+            self.execute_event(event)
+
+    def execute_event(self, event):
+        event.__run__()
+
