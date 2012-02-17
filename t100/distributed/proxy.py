316f39d8cd193086e046bafe8901774a2c84107f
Antonio Ribeiro Alves <alvejunior.antonio@gmail.com>
2012-02-17 18:04:44 +0100

    another one bit to bug!

--- t100/distributed/proxy.py
+++ t100/distributed/proxy.py
@@ -11,7 +11,7 @@ class Proxy(object):
         self.components = []
         self.ip = ip
         self.port = port
-        thread.start_new_thread(self.__receiver__, (self,ip,port))
+        thread.start_new_thread(self.__receiver__, (ip,port))
 
     def __receiver__(self, ip, port):
         HOST = ip                 # Symbolic name meaning all available interfaces
